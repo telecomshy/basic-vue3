4061a3c83925dd73ebe5b5b7362397451107cf56
telecomshy <telecomshy@189.cn>
2023-04-24 00:37:08 +0800

    修改登录页样式

--- src/views/login/Index.vue
+++ src/views/login/Index.vue
@@ -1,31 +1,35 @@
 <template>
     <div class="flex h-screen">
-        <div class="justify-align-center-[col] flex-grow bg-gradient-to-b from-[#e2e8f6] to-[#f1f6fc]">
-            <div class="w-[750px] mb-6">
+        <div class="justify-align-center-[col] flex-grow bg-gradient-to-b from-[#e2e8f6] to-[#f1f6fc] min-w-[640px]">
+            <div class="w-2/3 mb-6">
                 <div class="text-gray-700 text-3xl mb-1">翼随行短信平台</div>
                 <div class="text-gray-400 text-xl">welcome to use sms position system</div>
             </div>
-            <el-image class="w-[750px]" src="/login-bg.svg" fit="contain"></el-image>
+            <el-image class="w-2/3" src="/login-bg.svg" fit="contain"></el-image>
         </div>
-        <div id="login-container" class="justify-align-center-[col] w-[480px]">
+        <div id="login-container" class="justify-align-center-[col] w-1/3 min-w-[384px]">
             <div class="flex items-center mb-3">
-                <el-image src="/account-logo.png" fit="contain" class="mr-1 w-[32px]" />
+                <el-image src="/account-logo.png" fit="contain" class="mr-1 w-[32px]"/>
                 <span>用户登陆</span>
             </div>
             <el-form ref="loginFormRef" class="w-3/4" :model="loginForm" :rules="loginFormRules" label-position="top"
-                hide-required-asterisk>
-                <el-form-item label="用户名：" prop="username" required>
-                    <el-input v-model="loginForm.username" size="large" placeholder="请输入用户名" />
+                     hide-required-asterisk>
+                <el-form-item label="用户名：" prop="username">
+                    <el-input v-model="loginForm.username" size="large" placeholder="请输入用户名"/>
                 </el-form-item>
-                <el-form-item label="密码：" prop="password" required>
-                    <el-input v-model="loginForm.password" size="large" placeholder="请输入密码" />
+                <el-form-item label="密码：" prop="password">
+                    <el-input v-model="loginForm.password" size="large" placeholder="请输入密码" show-password/>
                 </el-form-item>
-                <el-form-item label="验证码：" prop="captcha" required>
-                    <el-input v-model="loginForm.captcha" size="large" placeholder="请输入验证码" />
-                    <el-image :src="captchaUrl" alt="" @click="getCaptcha" class="pointer" />
+                <el-form-item label="验证码：" prop="captcha">
+                    <el-input v-model="loginForm.captcha" size="large" placeholder="请输入验证码">
+                        <template #append>
+                            <el-image :src="captchaUrl" alt="" @click="getCaptcha"/>
+                        </template>
+                    </el-input>
                 </el-form-item>
                 <el-form-item>
-                    <el-button type="primary" @click="onSubmit(loginFormRef)">登录</el-button>
+                    <el-button class="w-full mt-4" type="primary" @click="onSubmit(loginFormRef)" size="large" auto-insert-space>登录
+                    </el-button>
                 </el-form-item>
             </el-form>
         </div>
@@ -33,12 +37,12 @@
 </template>
 
 <script setup lang="ts">
-import { reactive, onMounted, ref } from "vue";
-import { useAuthStore } from "@/store/auth";
-import { isAxiosError } from "axios";
-import { v4 as uuidv4 } from "uuid"
-import { $axios } from "@/utils/request";
-import type { FormInstance, FormRules } from 'element-plus'
+import {reactive, onMounted, ref} from "vue";
+import {useAuthStore} from "@/store/auth";
+import {isAxiosError} from "axios";
+import {v4 as uuidv4} from "uuid"
+import {$axios} from "@/utils/request";
+import type {FormInstance, FormRules} from 'element-plus'
 
 const loginFormRef = ref<FormInstance>()
 
@@ -50,9 +54,9 @@ const loginForm = reactive({
 })
 
 const loginFormRules = reactive<FormRules>({
-    username: [{ required: true, message: "请输入用户名", trigger: "blur" }],
-    password: [{ required: true, message: "请输入密码", trigger: "blur" }],
-    captcha: [{ required: true, message: "请输入验证码", trigger: "blur" }],
+    username: [{required: true, message: "请输入用户名", trigger: "blur"}],
+    password: [{required: true, message: "请输入密码", trigger: "blur"}],
+    captcha: [{required: true, message: "请输入验证码", trigger: "blur"}],
 })
 
 let captchaUrl = ref("")
@@ -63,7 +67,7 @@ async function getCaptcha() {
     const response = await $axios({
         method: "get",
         url: "/captcha",
-        params: { uuid: loginForm.uuid },
+        params: {uuid: loginForm.uuid},
         responseType: "blob"
     })
     captchaUrl.value = URL.createObjectURL(response.data)
@@ -85,7 +89,7 @@ const onSubmit = async (form: FormInstance | undefined) => {
             } catch (error) {
                 // 如果用户名、密码或者验证码不对，统一返回401错误，另外，如果用户名和密码为空，会返回422错误，422统一由拦截器处理
                 if (isAxiosError(error) && error?.response) {
-                    const { status, data } = error.response
+                    const {status, data} = error.response
                     if (status === 401) {
                         // TODO 在合适的地方显示登陆错误消息
                         alert(data["detail"])
@@ -104,6 +108,6 @@ const onSubmit = async (form: FormInstance | undefined) => {
     position: absolute;
     left: auto;
     right: 0;
-    top: -27px;
+    top: -25px;
 }
 </style>
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,33 +2,27 @@
 import plugin from "tailwindcss/plugin"
 
 export default {
-  content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
-  theme: {
-    extend: {
-      fontFamily: {
-        "element-fonts": ['Helvetica Neue', 'Helvetica', 'PingFang SC', 'Hiragino Sans GB', 'Microsoft YaHei', '微软雅黑', 'Arial', 'sans - serif']
-      }
-    },
-  },
-  plugins: [
-    plugin(function ({ matchComponents }) {
-      const ruleObj = {
-        "display": "flex",
-        "justify-content": "center",
-        "align-items": "center"
-      }
-
-      matchComponents({
-        "justify-align-center": value => {
-          if (value === "col") {
-            ruleObj["flex-direction"] = "column"
-          } else {
-            ruleObj["flex-direction"] = value
-          }
-          return ruleObj
+    content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'], theme: {
+        extend: {
+            fontFamily: {
+                "element-fonts": ['Helvetica Neue', 'Helvetica', 'PingFang SC', 'Hiragino Sans GB', 'Microsoft YaHei', '微软雅黑', 'Arial', 'sans - serif']
+            }
+        },
+    }, plugins: [plugin(function ({matchComponents}) {
+        const ruleObj = {
+            "display": "flex", "justify-content": "center", "align-items": "center"
         }
-      })
-    }),
-  ],
+
+        matchComponents({
+            "justify-align-center": value => {
+                if (value === "col") {
+                    ruleObj["flex-direction"] = "column"
+                } else {
+                    ruleObj["flex-direction"] = value
+                }
+                return ruleObj
+            }
+        })
+    }),],
 }
 
