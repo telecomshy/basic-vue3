7e5ea1a5e4787f2841dba9d8d461da7410622600
telecomshy <18907190693@189.cn>
2023-05-06 17:31:08 +0800

    取消size等变量，直接写入模板

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,9 @@
 <template>
-  <el-config-provider :size="size" :z-index="zIndex" :locale="local">
-    <router-view :key="$route.fullPath"></router-view>
-  </el-config-provider>
+    <el-config-provider size="default" :z-index="3000" :locale="zhCn">
+        <router-view :key="$route.fullPath"></router-view>
+    </el-config-provider>
 </template>
 
 <script setup lang="ts">
 import zhCn from 'element-plus/lib/locale/lang/zh-cn'
-
-const zIndex = 3000
-const size = "default"
-const local = zhCn
 </script>
